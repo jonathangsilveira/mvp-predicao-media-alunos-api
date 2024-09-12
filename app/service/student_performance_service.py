141b71237fcbe8e2fd11689981e7e8c62982716b
Jonathan Giorgi Silveira <jonathangsilveira@gmail.com>
2024-09-11 22:33:33 -0300

    Apply ML module

--- app/service/student_performance_service.py
+++ app/service/student_performance_service.py
@@ -2,36 +2,41 @@ from typing import Optional
 import pandas as pd
 import numpy as np
 
+from app.ml_model.pipeline_delegate import PipelineDelegate
 from app.model.grade import Grade
 from app.model.pipeline.pipeline import PipelineLoader
 
 from app.model.student_performance import StudentPerformance
 from app.repository.student_performance_repository import StudentPerformanceRepository
 from app.schema.student_data_body import StudentDataBody
-from app.schema.student_grade_prediction_response import StudentGradePredictionResponse
 from app.service.grade_classification_mapper import GradeMapper
+
+from app.ml_model.preprocessing import InputReshapePreProcessor
 
 PIPELINE_FILE_PATH = './machine_learning/pipelines/svm_student_performance_pipeline.pkl'
 
 class StudentPerformanceService:
 
     def __init__(self, repository: StudentPerformanceRepository, 
-                 grade_mapper: GradeMapper) -> None:
+                 grade_mapper: GradeMapper, preProcessor: InputReshapePreProcessor, 
+                 pipeline: PipelineDelegate) -> None:
         self.repository = repository
         self.grade_mapper = grade_mapper
+        self.preProcessor = preProcessor
+        self.pipeline = pipeline
 
     def predict(self, studentData: StudentDataBody) -> Grade:
-        input = self.__pre_process__(studentData)
+        input = self.preProcessor.process_reshape(studentData)
 
-        loader = PipelineLoader()
-        pipeline = loader.load_pipeline(filepath=PIPELINE_FILE_PATH)
+        self.pipeline.load(PIPELINE_FILE_PATH)
         
-        prediction = pipeline.predict(input)
+        prediction = self.pipeline.predict(input)
         outcome = int(prediction[0])
+        grade = self.grade_mapper.map(outcome)
 
-        self.__insert_student_performance__(studentData, outcome)
+        self.__insert_student_performance__(studentData, grade)
 
-        return self.grade_mapper.map(classification=outcome)
+        return grade
     
     def get_all_performances(self) -> list[StudentPerformance]:
         return self.repository.all()
@@ -42,41 +47,7 @@ class StudentPerformanceService:
     def remove_student_performance(self, student_id: int) -> None:
         self.repository.remove(student_id=student_id)
         
-    def __pre_process__(self, studentData: StudentDataBody) -> list[list]:
-        x_input = np.array([
-            studentData.age, 
-            studentData.gender_code, 
-            studentData.ethnicity_code,
-            studentData.parental_education_level,
-            studentData.weekly_study_time,
-            studentData.absence_count,
-            studentData.tutoring_status,
-            studentData.parental_support_level,
-            studentData.extracurricular,
-            studentData.sports,
-            studentData.music,
-            studentData.volunteering
-        ])
-        dataset: list[list] = x_input.reshape(1, -1)
-        columns = (
-            'age',
-            'gender_code',
-            'ethnicity_code',
-            'parental_education_level',
-            'weekly_study_time',
-            'absence_count',
-            'tutoring_status',
-            'parental_support_level',
-            'extracurricular',
-            'sports',
-            'music',
-            'volunteering'
-        )
-        dataframe = pd.DataFrame(data=dataset, columns=columns)
-        data: list[list] = dataframe.to_numpy()
-        return data[:,0:11]
-    
-    def __insert_student_performance__(self, studentData: StudentDataBody, prediction: int) -> None:
+    def __insert_student_performance__(self, studentData: StudentDataBody, grade: Grade) -> None:
         performance = StudentPerformance(
             age=studentData.age, 
             gender_code=studentData.gender_code, 
@@ -90,6 +61,6 @@ class StudentPerformanceService:
             sports=studentData.sports,
             music=studentData.music,
             volunteering=studentData.volunteering,
-            grade_classification=self.grade_mapper.map(prediction)
+            grade_classification=grade
         )
         self.repository.insert(performance)
