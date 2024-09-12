deea7fac92572cda480e3d88bce8f24b4e6c9474
Jonathan Giorgi Silveira <jonathangsilveira@gmail.com>
2024-09-11 22:32:13 -0300

    Remove unused files

--- app/model/loader/cvs_dataset_loader.py
+++
@@ -1,8 +0,0 @@
-from pandas import (DataFrame, read_csv)
-
-from dataset_loader import DatasetLoader
-
-class CVSDatasetLoader(DatasetLoader):
-
-    def load(self, source: any, attributes: list[str]) -> DataFrame:
-        return read_csv(str(source), skiprows=0, delimiter=',')
--- app/model/loader/dataset_loader.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-from pandas import DataFrame
-
-class DatasetLoader(ABC):
-
-    @abstractmethod
-    def load(self, source: any, attributes: list[str]) -> DataFrame:
-        ...
