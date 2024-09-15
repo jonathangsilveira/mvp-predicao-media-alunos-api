2c9efed9008ae971b02799a3d9630c53adbc8efd
Jonathan Giorgi Silveira <jonathangsilveira@gmail.com>
2024-09-15 10:35:55 -0300

    Modify response model

--- app/schema/student_performance_response.py
+++ app/schema/student_performance_response.py
@@ -3,17 +3,17 @@ from pydantic import BaseModel
 class StudentPerformanceResponse(BaseModel):
     student_id: int = 1
     age: int = 15
-    gender_code: int = 1 
-    ethnicity_code: int = 1
-    parental_education_level: int = 1 
-    weekly_study_time: int = 10
+    gender: str = 'Masculino' 
+    ethnicity: str = 'Afrodescendente'
+    parental_education_level: str = 'Ensino médio' 
+    weekly_study_time: str = '10h'
     absence_count: int = 10
-    tutoring_status: int = 0
-    parental_support_level: int = 3 
-    extracurricular: int = 0
-    sports: int = 1
-    music: int = 0
-    volunteering: int = 0
+    tutoring_status: bool = False
+    parental_support_level: str = 'Moderado' 
+    extracurricular: bool = False
+    sports: bool = True
+    music: bool = True
+    volunteering: bool = False
     grade_classification: str = 'C'
 
 class StudentPerformancesResponse(BaseModel):
