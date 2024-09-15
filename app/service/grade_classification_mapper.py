91587ed446143f50d431fb3bec7d0d3605d07776
Jonathan Giorgi Silveira <jonathangsilveira@gmail.com>
2024-09-15 10:52:38 -0300

    Classify grade by float values

--- app/service/grade_classification_mapper.py
+++ app/service/grade_classification_mapper.py
@@ -3,12 +3,14 @@ from app.model.grade import Grade
 
 class GradeMapper:
 
-    def map(self, classification: int) -> Grade:
-        grades = {
-            0: Grade.A,
-            1: Grade.B,
-            2: Grade.C,
-            3: Grade.D,
-            4: Grade.E
-        }
-        return grades[classification]
+    def map(self, classification: float) -> Grade:
+        if classification < 1.0:
+            return Grade.A
+        elif classification < 2.0:
+            return Grade.B
+        elif classification < 3.0:
+            return Grade.C
+        elif classification < 4.0:
+            return Grade.D
+        else:
+            return Grade.E
